a338cdc96bffc009fbd37679d3d0cbbe725b1139
Antoine Cheron <cheron.antoine@gmail.com>
2018-05-08 14:25:48 +0200

    [WIP] PersonApi and main

--- src/main/java/fr/cheron/antoine/reactivewebserver/webflux/Main.java
+++ src/main/java/fr/cheron/antoine/reactivewebserver/webflux/Main.java
@@ -1,4 +1,9 @@
 package fr.cheron.antoine.reactivewebserver.webflux;
 
 public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
 }
--- src/main/java/fr/cheron/antoine/reactivewebserver/webflux/PersonApi.java
+++ src/main/java/fr/cheron/antoine/reactivewebserver/webflux/PersonApi.java
@@ -1,4 +1,73 @@
 package fr.cheron.antoine.reactivewebserver.webflux;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
+
+import fr.cheron.antoine.reactivewebserver.exceptions.NotFoundResourceException;
+import fr.cheron.antoine.reactivewebserver.services.PersonService;
+import fr.cheron.antoine.reactivewebserver.json.Json;
+import fr.cheron.antoine.reactivewebserver.utils.Responses;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
 public class PersonApi {
+
+  // Path variables
+  public static final String PERSON_ID_PATH_VARIABLE = "id";
+
+  // Paths
+  public static final String BASE_PATH = "";
+  public static final String ONE_PERSON = BASE_PATH + "/{" + PERSON_ID_PATH_VARIABLE + "}";
+
+  private final PersonService personService;
+
+  public final RouterFunction routerFunction;
+
+  public PersonApi(PersonService personService) {
+    this.personService = personService;
+
+    this.routerFunction = RouterFunctions.
+      route(GET(BASE_PATH), this::getAllPersons).
+      andRoute(GET(ONE_PERSON), this::getOnePersonById).
+      andRoute(POST(ONE_PERSON), this::updateOnePersonById).
+      andRoute(DELETE(ONE_PERSON), this::deleteOnePersonById).
+      andRoute(POST(BASE_PATH), this::createOnePerson);
+
+  }
+
+  private Mono<ServerResponse> getAllPersons(ServerRequest request) {
+    return this.personService.
+      list().
+      collectList().
+      flatMap(Json::write).
+      flatMap(Responses::ok).
+      onErrorResume(JsonProcessingException.class, Responses::internalServerError);
+  }
+
+  private Mono<ServerResponse> getOnePersonById(ServerRequest request) {
+    return this.personService.
+      findById(request.pathVariable(PERSON_ID_PATH_VARIABLE)).
+      flatMap(Json::write).
+      flatMap(Responses::ok).
+      onErrorResume(NotFoundResourceException.class, Responses::notFound).
+      onErrorResume(JsonProcessingException.class,Responses::internalServerError);
+  }
+
+  private Mono<ServerResponse> updateOnePersonById(ServerRequest request) {
+    return request.bodyToMono(String.class).
+    return this.personService.
+      updateOneById(request.pathVariable(PERSON_ID_PATH_VARIABLE)).
+      flatMap(Json::write).
+      flatMap(Responses::ok).
+      onErrorResume(NotFoundResourceException.class, Responses::notFound).
+      onErrorResume(JsonProcessingException.class,Responses::internalServerError);
+  }
+
+
 }
